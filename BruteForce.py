e2a301d25f1d32db00ea16506db31104ab7e8b05
Cameron McNamara <cameronmcnamara@sandiego.edu>
2026-02-11 10:09:10 -0800

    Efficent

--- BruteForce.py
+++ BruteForce.py
@@ -1,6 +1,13 @@
 import math
 
 
+def main():
+    nums = list(
+        map(int, input("Enter Array seperated by space (i.e: 1 6 2 3 6): ").split())
+    )
+    print("Max Sum Sum Array: ", maxSubArrayBruteForce(nums))
+
+
 def maxSubArrayBruteForce(nums):
     ans = -math.inf
     for i in range(len(nums)):
@@ -9,3 +16,7 @@ def maxSubArrayBruteForce(nums):
             cur_sum += nums[j]
             ans = max(ans, cur_sum)
     return ans
+
+
+if __name__ == "__main__":
+    main()
---
+++ Efficent.py
@@ -0,0 +1,20 @@
+import math
+
+
+def main():
+    nums = list(
+        map(int, input("Enter Array seperated by space (i.e: 1 6 2 3 6): ").split())
+    )
+    print("Max Sum Sum Array: ", maxSubArrayEfficent(nums))
+
+
+def maxSubArrayEfficent(nums):
+    cur_max, max_so_far = 0, -math.inf
+    for c in nums:
+        cur_max = max(c, cur_max + c)
+        max_so_far = max(max_so_far, cur_max)
+    return max_so_far
+
+
+if __name__ == "__main__":
+    main()
