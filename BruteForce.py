d6ac9e8dc5ee5263cba7513ffcb952365160d0c1
Cameron McNamara <cameronmcnamara@sandiego.edu>
2026-02-11 09:22:23 -0800

    BruteFOrce

---
+++ BruteForce.py
@@ -0,0 +1,11 @@
+import math
+
+
+def maxSubArrayBruteForce(nums):
+    ans = -math.inf
+    for i in range(len(nums)):
+        cur_sum = 0
+        for j in range(i, len(nums)):
+            cur_sum += nums[j]
+            ans = max(ans, cur_sum)
+    return ans
