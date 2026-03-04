6fa107a4cfc9364a149b794aa814cd1e7b9e5b1a
Cameron McNamara <cameronmcnamara@sandiego.edu>
2026-03-04 08:08:03 -0800

    q2ha2

--- HA-2/Q2-Classroom.py
+++ HA-2/Q2-Classroom.py
@@ -1,5 +1,6 @@
 import heapq
 
+
 def min_classrooms(intervals):
     # sort intervals by start time
     intervals.sort(key=lambda x: x[0])
@@ -17,13 +18,13 @@ def min_classrooms(intervals):
 
 
 # Example
-intervals = [[30,75],[0,50],[60,150],[80,120],[55,65]]
+intervals = [[30, 75], [0, 50], [60, 150], [80, 120], [55, 65]]
 print(min_classrooms(intervals))
 
-#test cases
-test1 = [[1,2], [3,4], [5,6]] # no overlap, return 1
-test2 = [[1,4], [2,5], [7,9]] # 1 overlap, return 2
-test3 =[[1,10], [2,9], [3,8], [4,7]] # everything overlaps, return 4
+# test cases
+test1 = [[1, 2], [3, 4], [5, 6]]  # no overlap, return 1
+test2 = [[1, 4], [2, 5], [7, 9]]  # 1 overlap, return 2
+test3 = [[1, 10], [2, 9], [3, 8], [4, 7]]  # everything overlaps, return 4
 print("test1: " + str(min_classrooms(test1)) + " expected: 1")
 print("test2: " + str(min_classrooms(test2)) + " expected: 2")
 print("test3: " + str(min_classrooms(test3)) + " expected: 4")
